6e8f25ddb75e3b2d85886e981643fbb78c020c69
luckychacha <358140284@qq.com>
2024-01-02 01:20:04 +0800

    Feat: Max Block Author Inherent Data Provider implement.

--- primitives/max_block_author/src/lib.rs
+++ primitives/max_block_author/src/lib.rs
@@ -1,9 +1,14 @@
 
+use codec::{Encode, Decode};
+use scale_info::TypeInfo;
+use sp_inherents::InherentIdentifier;
+
+
 #[derive(Encode, Decode, sp_core::RuntimeDebug, Clone, PartialEq, TypeInfo)]
 pub struct MaxBlockAuthorInherentDataProvider {
 	times: u32,
 }
-pub const INHERENT_IDENTIFIER: InherentIdentifier = *b"maxblockauthor";
+pub const INHERENT_IDENTIFIER: InherentIdentifier = *b"maxblcka";
 
 impl MaxBlockAuthorInherentDataProvider {
 	fn new(times: u32) -> Self {
