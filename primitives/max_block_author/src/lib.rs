e8e39fe7ac229de32d8a515d3ac3aa0736b953b6
luckychacha <358140284@qq.com>
2024-01-04 17:49:03 +0800

    Feat: Add awaiting_inherent_processing in local struct.

--- primitives/max_block_author/src/lib.rs
+++ primitives/max_block_author/src/lib.rs
@@ -6,12 +6,13 @@ use sp_inherents::InherentIdentifier;
 
 #[derive(Encode, Decode, sp_core::RuntimeDebug, Clone, PartialEq, TypeInfo)]
 pub struct MaxBlockAuthorInherentDataProvider {
-	times: u32,
+	pub times: u32,
+	pub pub awaiting_inherent_processing: bool,
 }
 pub const INHERENT_IDENTIFIER: InherentIdentifier = *b"maxblcka";
 
 impl MaxBlockAuthorInherentDataProvider {
-	fn new(times: u32) -> Self {
-		Self { times }
+	fn new(times: u32, awaiting_inherent_processing: bool) -> Self {
+		Self { times, awaiting_inherent_processing }
 	}
 }
